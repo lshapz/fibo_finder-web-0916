e779a955091bfd717b3f2e609d34de0ec17ee4cd
lshapz <laurashapiro01@gmail.com>
2016-12-22 17:18:24 -0500

    Done.

--- fibo_finder.rb
+++ fibo_finder.rb
@@ -1,3 +1,14 @@
 def fibo_finder(n)
+  array = [0, 1]
+
+  n.times do |x|
+    last_index = array.length-1 
+    second_last = array.length-2
+    array.push(array[last_index] + array[second_last])
+  end 
+
+  array[n]
+
   # code goes here
 end
+
